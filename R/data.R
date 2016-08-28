da6bf8aaa374ddb7f6ec71d2ace8ed6a36bf0de0
nevrome <clemens@nevrome.de>
2016-08-28 14:53:04 +0200

    added documentation for datasets KT_spits and KT_squarecorners

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Niveau measurements of a fictional excavation KT
+#'
+#' A dataset containing coordinates of niveau measurements of a fictional excavation KT with
+#' 4 spits
+#'
+#' @format A data frame with 304 rows and 4 variables:
+#' \itemize{
+#'   \item id: IDs of individual measurements with the information about to which level
+#'   they belong
+#'   \item x: x axis coordinates of measurements
+#'   \item y: y axis coordinates of measurements
+#'   \item z: z axis coordinates of measurements
+#' }
+#'
+#' @name KT_spits
+NULL
+
+#' Corner points of a 1m*1m raster within the trench of a fictional excavation KT
+#'
+#' A dataset containing horizontal coordinates of corner points of a 1m*1m raster within
+#' a rectangular trench (corner points of squares)
+#'
+#' @format A data frame with 63 rows and 2 variables:
+#' \itemize{
+#'   \item x: x axis coordinates of corner points
+#'   \item y: y axis coordinates of corner points
+#' }
+#'
+#' @name KT_squarecorners
+NULL
