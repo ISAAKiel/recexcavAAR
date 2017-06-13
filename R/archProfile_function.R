1e169128b0b36b08251a1641201a1b4993fa5024
MoritzMennenga <mennenga@nihk.de>
2017-06-13 10:09:16 +0200

    travis_error

--- R/archProfile_function.R
+++ R/archProfile_function.R
@@ -6,7 +6,7 @@
 #' @param view_col Name of the profile containing the viewing direction
 #' @return SpatialDataFrame with the new coordinates
 #' @examples
-#'
+#' library(sp)
 #' fotogram_sdf <- SpatialPointsDataFrame(coords = fotogram_pts[ ,c(1,2,3)], data = fotogram_pts, proj4string = CRS('+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs'))
 #' profile <- archProfile(fotogram_pts = fotogram_sdf, profile_col = "pr", view_col = "view")
 #'
