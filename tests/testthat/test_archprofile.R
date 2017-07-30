9d70320bb930fcab29512a87c3282c5f10e98ded
nevrome <clemens@nevrome.de>
2017-07-30 18:13:43 +0200

    minimalistic test for archprofile

---
+++ tests/testthat/test_archprofile.R
@@ -0,0 +1,27 @@
+context("Tests of function archprofile")
+
+#### prepare test data ####
+# archprofile_input_1 <- sp::SpatialPointsDataFrame(
+#   coords = fotogram_pts[, c(1,2,3)],
+#   data = fotogram_pts,
+#   proj4string = sp::CRS("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")
+# )
+# save(archprofile_input_1, file = "tests/testthat/data/archprofile_input_1.RData")
+# load("tests/testthat/data/archprofile_input_1.RData")
+load("data/archprofile_input_1.RData")
+
+# archprofile_output_1_correct <- archprofile(
+archprofile_output_1 <- archprofile(
+  fotogram_pts = archprofile_input_1,
+  profile_col = "pr",
+  view_col = "view"
+)
+# save(archprofile_output_1_correct, file = "tests/testthat/data/archprofile_output_1_correct.RData")
+# load("tests/testthat/data/archprofile_output_1_correct.RData")
+load("data/archprofile_output_1_correct.RData")
+
+test_that(
+  "the output of archprofile is the same as before the code refactoring for a very limited example",  {
+    expect_identical(archprofile_output_1, archprofile_output_1_correct)
+  }
+)
