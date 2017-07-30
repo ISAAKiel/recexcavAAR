9d54a55cd3d12731b420a025e288bf3d87a6c5e4
nevrome <clemens@nevrome.de>
2017-07-30 19:21:21 +0200

    some code refactoring for archprofile

--- R/archProfile_function.R
+++ R/archProfile_function.R
@@ -29,43 +29,26 @@
 #' )
 #'
 #' @export
-archprofile <- function(fotogram_pts, profile_col, view_col, view = "projected", direction = "horizontal"){
-
-
+archprofile <- function(fotogram_pts, profile_col, view_col,
+                        view = "projected",  direction = "horizontal") {
   #Die Datei muss zwei Spalten haben, zum einen die mit der Gruppierung
   #für die Profile (z.B. Profilnummer), und (optional) eine
   #Spalte mit der view von N/S/E/W, damit das Profil richtig gedreht werden kann
-  #Spalte mit dem Namen raussuchen und den Spaltenindex merken
-  profile_id <- 1
-  while(profile_id<length(colnames(fotogram_pts@data)) &&
-        colnames(fotogram_pts@data)[profile_id] != profile_col)
-  {
-    profile_id <- profile_id+1
-  }
 
-  #Spalte mit dem NAmen raussuchen und den Spaltenindex merken
-  view_id <- 1
-  while(view_id<length(colnames(fotogram_pts@data)) &&
-        colnames(fotogram_pts@data)[view_id] != view_col)
-  {
-    view_id <- view_id+1
-  }
-
-
-  #Die Punkte mssen umden Mittelpunkt rotiert werden,
+  #Die Punkte müssen umden Mittelpunkt rotiert werden,
   #sodass sie parallel zur x achse liegen
-  #Bedingung alle Pkt mssen mglichst auf einer Ebene liegen
-  #Dazu Koordinaten in Dataframe schreiben
-  coord <- data.frame(fotogram_pts@coords[ ,1],
-                      fotogram_pts@coords[ ,2],
-                      fotogram_pts@coords[ ,3],
-                      fotogram_pts@data[ ,profile_id],
-                      fotogram_pts@data[, view_id])
-  colnames(coord) <- c("x", "y", "z", "pr","view")
-
+  #Bedingung alle Pkt müssen möglichst auf einer Ebene liegen
+  #Dazu Koordinaten in data.frame schreiben
+  coord <- data.frame(
+    x = fotogram_pts@coords[, 1],
+    y = fotogram_pts@coords[, 2],
+    z = fotogram_pts@coords[, 3],
+    pr = fotogram_pts@data[, profile_col],
+    view = fotogram_pts@data[, view_col]
+  )
 
   #Jetzt muss jedes Profil einzeln bearbeitet werden
-  #Nas raus
+  #NAs raus
   coord <- na.omit(coord)
   #Alle Namen der Profile ermitteln, da sich daraus die
   #Anzahl der Schleifenduchläufe bestimmt
@@ -81,13 +64,12 @@ archprofile <- function(fotogram_pts, profile_col, view_col, view = "projected",
   coord_export$x <- 0
   coord_export$y <- 0
 
-
   i <- 1
   #Jetzt jedes Profil durchlaufen
-  while (i <= length(prnames)){
+  while (i <= length(prnames)) {
     #Alle Daten des iten Profils auslesen und in temporären dataframe schreiben
-    coord_proc <- coord[which (coord$pr==prnames[i]), ]
-    #nun die Mittelline bestimmen -> lieare Regression
+    coord_proc <- coord[which(coord$pr == prnames[i]),]
+    #nun die Mittelline bestimmen -> lineare Regression
     #Entspricht dem BKS in AutoCAD, allerdings in 2D.
     #Je gerader die Profile, desto genauer. Man könnte auch mit Matritzen arbeiten,
     #ich denke aber, dass das übertrieben ist.
@@ -114,39 +96,35 @@ archprofile <- function(fotogram_pts, profile_col, view_col, view = "projected",
     #4. m = + view = N/W -> gegen Uhrzeigersinn
     #Dafür die Drehwinkel bestimmen (rad -> deg)
 
+    view_proc <- coord_proc$view[1]
 
-
-    if (slope < 0 && coord_proc$view[1] == "N" ||
-        slope < 0 && coord_proc$view[1] == "E"){
+    if (slope < 0 && view_proc %in% c("N", "E")) {
       slope_deg <- 180 - abs((atan(slope) * 180) / pi) * -1
-    } else if (slope < 0 && coord_proc$view[1] == "S" ||
-               slope < 0 && coord_proc$view[1] == "W"){
-      slope_deg <- abs((atan(slope)*180)/pi)
-    } else if (slope > 0 && coord_proc$view[1] == "S" ||
-               slope > 0 && coord_proc$view[1] == "E") {
-      slope_deg <- ((atan(slope)*180)/pi)*-1
-    } else if (slope > 0 && coord_proc$view[1] == "N" ||
-               slope > 0 && coord_proc$view[1] == "W") {
-      slope_deg <- 180 - ((atan(slope)*180)/pi)
-    } else if (slope == 0 && coord_proc$view[1] == "N" ){
+    } else if (slope < 0 && view_proc %in% c("S", "W")) {
+      slope_deg <- abs((atan(slope) * 180) / pi)
+    } else if (slope > 0 && view_proc %in% c("S", "E")) {
+      slope_deg <- ((atan(slope) * 180) / pi) * -1
+    } else if (slope > 0 && view_proc %in% c("N", "W")) {
+      slope_deg <- 180 - ((atan(slope) * 180) / pi)
+    } else if (slope == 0 && view_proc == "N") {
       slope_deg <- 180
-    } else if (slope == 0 && coord_proc$view[1] == "N" ){
+    } else if (slope == 0 && view_proc == "N") {
       slope_deg <- 0
     }
 
     #Nun den Drehpunkt bestimmen.
     #X-Wert ist die Mitte zwischen den x-Koordinaten
-    center_x <- sum(coord_proc$x)/length(coord_proc$x)
+    center_x <- sum(coord_proc$x) / nrow(coord_proc)
     #Y-Wert
-    center_y <- sum(coord_proc$y)/length(coord_proc$y)
+    center_y <- sum(coord_proc$y) / nrow(coord_proc)
     #Nun um diesen Punkt drehen
 
     coord_trans <- coord_proc
     #Die Spalte view fällt raus
     coord_trans$view <- NULL
-    #Für jeden Punkt des Profils mittels translation
-    #und rotation den neuen Punkt bestimmen
-    for (z in 1:nrow(coord_proc)){
+    #Für jeden Punkt des Profils mittels Translation
+    #und Rotation den neuen Punkt bestimmen
+    for (z in 1:nrow(coord_proc)) {
       coord_trans[z,] <- c(
         center_x + (coord_proc$x[z] - center_x) *
           cos(slope_deg / 180 * pi) - sin(slope_deg / 180 * pi) *
@@ -154,107 +132,97 @@ archprofile <- function(fotogram_pts, profile_col, view_col, view = "projected",
         center_y + (coord_proc$x[z] - center_x) *
           sin(slope_deg / 180 * pi) + (coord_proc$y[z] - center_y) *
           cos(slope_deg / 180 * pi),
-        (coord_proc$z[z]+center_y-mean(coord_proc$z)),
-        coord_proc$pr[z])
+        coord_proc$z[z] + center_y - mean(coord_proc$z),
+        coord_proc$pr[z]
+      )
       #http://www.matheboard.de/archive/460078/thread.html
-
     }
 
-    if (view == "surface"){
+    if (view == "surface") {
       #Jetzt das ganze für die z-Achse, um eine Kippung des Profils zu minimieren
 
-      z_yw <- c(coord_trans$y -  min(c(coord_trans$y,coord_trans$z)))
-      z_zw <- c(coord_trans$z -  min(c(coord_trans$y,coord_trans$z)))
-
+      z_yw <- c(coord_trans$y - min(c(coord_trans$y, coord_trans$z)))
+      z_zw <- c(coord_trans$z - min(c(coord_trans$y, coord_trans$z)))
       z_fm <- lm(z_zw ~ z_yw)
-
 
       #Steigungswinkel berechnen
       z_slope <- coef(z_fm)[2]
 
-      if (z_slope < 0){
-
-        z_slope_deg <- (90 - abs((atan(z_slope)*180)/pi))*-1
-      } else if (z_slope > 0){
-        z_slope_deg <- 90 - ((atan(z_slope)*180)/pi)
-      } else if (z_slope == 0){
+      if (z_slope < 0) {
+        z_slope_deg <- -(90 - abs((atan(z_slope) * 180) / pi))
+      } else if (z_slope > 0) {
+        z_slope_deg <- 90 - ((atan(z_slope) * 180) / pi)
+      } else if (z_slope == 0) {
         z_slope_deg <- 0
       }
 
-
-      z_center_y <- sum(coord_trans$y)/length(coord_trans$y)
-      z_center_z <- sum(coord_trans$z)/length(coord_trans$z)
+      z_center_y <- sum(coord_trans$y) / nrow(coord_trans)
+      z_center_z <- sum(coord_trans$z) / nrow(coord_trans)
 
       z_coord <- coord_trans
 
-      for (z in 1:nrow(z_coord)){
+      for (z in 1:nrow(z_coord)) {
         z_coord[z,] <- c(
           coord_trans$x[z],
           z_center_y + (coord_trans$y[z] - z_center_y) * cos(z_slope_deg / 180 * pi) -
             (coord_trans$z[z] - z_center_z) * sin(z_slope_deg / 180 * pi),
           z_center_z + (coord_trans$y[z] - z_center_y) * sin(z_slope_deg / 180 * pi) +
             (coord_trans$z[z] - z_center_z) * cos(z_slope_deg / 180 * pi),
-
-          coord_trans$pr[z])
+          coord_trans$pr[z]
+        )
         #http://www.matheboard.de/archive/460078/thread.html
-
       }
       coord_trans <- z_coord
     }
 
-
-    if (direction == "original"){
-      y_xw <- c(coord_trans$x -  min(c(coord_trans$x,coord_trans$z)))
-      y_zw <- c(coord_trans$z -  min(c(coord_trans$x,coord_trans$z)))
-
+    if (direction == "original") {
+      y_xw <- c(coord_trans$x -  min(c(coord_trans$x, coord_trans$z)))
+      y_zw <- c(coord_trans$z -  min(c(coord_trans$x, coord_trans$z)))
       y_fm <- lm(y_zw ~ y_xw)
 
 
       #Der Drehwinkel ist in diesem Falle der negative Drehwinkel des ersten Schrittes
-      y_slope_deg <- slope_deg * -1
+      y_slope_deg <- -slope_deg
 
-
-      y_center_x <- sum(coord_trans$x)/length(coord_trans$x)
-      y_center_z <- sum(coord_trans$z)/length(coord_trans$z)
+      y_center_x <- sum(coord_trans$x) / nrow(coord_trans)
+      y_center_z <- sum(coord_trans$z) / nrow(coord_trans)
 
       y_coord <- coord_trans
 
-      for (z in 1:nrow(y_coord)){
+      for (z in 1:nrow(y_coord)) {
         y_coord[z,] <- c(
           y_center_x + (coord_trans$x[z] - y_center_x) * cos(y_slope_deg / 180 * pi) -
             (coord_trans$z[z] - y_center_z) * sin(y_slope_deg / 180 * pi),
           coord_trans$y[z],
           y_center_z + (coord_trans$x[z] - y_center_x) * sin(y_slope_deg / 180 * pi) +
             (coord_trans$z[z] - y_center_z) * cos(y_slope_deg / 180 * pi),
-          coord_trans$pr[z])
+          coord_trans$pr[z]
+        )
         #http://www.matheboard.de/archive/460078/thread.html
-
       }
       coord_trans <- y_coord
     }
 
 
     #Dann passend in den dataframe speichern
-    coord_export[which (coord$pr==prnames[i]), ] <- coord_trans
-    i<-i+1
+    coord_export[which(coord$pr == prnames[i]),] <- coord_trans
+    i <- i + 1
     #temporären dataframe löschen
     coord_proc <- NULL
     coord_trans <- NULL
 
-    if (view == "surface"){
+    if (view == "surface") {
       z_coord <- NULL
     }
 
   }
 
-
-
-
   #Das ganze zu einem Spatialdataframe machen
-  export <- SpatialPointsDataFrame(coords=coord_export[,c(1,3)],
-                                   data = coord_export,
-                                   proj4string = (fotogram_pts@proj4string))
+  export <- SpatialPointsDataFrame(
+    coords = coord_export[, c(1, 3)],
+    data = coord_export,
+    proj4string = fotogram_pts@proj4string
+  )
 
-
-return(export)
+  return(export)
 }
