ab33768c43c8110663c71ba5e324ff5a5a4fc8df
MoritzMennenga <mennenga@nihk.de>
2017-08-13 13:14:32 +0200

    travis

--- R/archProfile_oneprofile_function.R
+++ R/archProfile_oneprofile_function.R
@@ -41,13 +41,7 @@ archoneprofile <- function(fotogram_pts, view_col, x, y, z,
   #therefore they are parallel to the x axis
   #All points need to be (theortical) on a plane
   #At first writing the coordinates and attributes in a dataframe
-  #Test if data contains only allowed view directions
-  if(identical(setdiff(levels(as.factor(fotogram_pts[,view_col])), c("E","N","S","W")),character(0)))
-  {
-    #Everything is ok
-  } else {
-    stop('View direction is only allowed to contain N, S, E, W')
-  }
+
 
 
   #If input is a spatialdataframe
