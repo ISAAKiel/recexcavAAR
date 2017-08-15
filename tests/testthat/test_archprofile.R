d754374441fa876ca22b1bde253d1e2e3844c9e8
Moritz Mennenga <mennenga@nihk.de>
2017-08-15 13:23:32 +0200

    archprofile test for all slopes and all views

--- tests/testthat/test_archprofile.R
+++ tests/testthat/test_archprofile.R
@@ -2,8 +2,8 @@ context("Tests of function archprofile")
 
 #### prepare test data ####
 # archprofile_input_1 <- sp::SpatialPointsDataFrame(
-#   coords = fotogram_pts[, c(1,2,3)],
-#   data = fotogram_pts,
+#   coords = archprofile_test[, c(1,2,3)],
+#   data = archprofile_test,
 #   proj4string = sp::CRS("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")
 # )
 # save(archprofile_input_1, file = "tests/testthat/data/archprofile_input_1.RData")
@@ -14,9 +14,9 @@ load("data/archprofile_input_1.RData")
 archprofile_output_1 <- archprofile(
   fotogram_pts = archprofile_input_1,
   profile_col = "pr",
-  view_col = "view"
+  view_col = "pr1"
 )
-# save(archprofile_output_1_correct, file = "tests/testthat/data/archprofile_output_1_correct.RData")
+#save(archprofile_output_1_correct, file = "tests/testthat/data/archprofile_output_1_correct.RData")
 # load("tests/testthat/data/archprofile_output_1_correct.RData")
 load("data/archprofile_output_1_correct.RData")
 
